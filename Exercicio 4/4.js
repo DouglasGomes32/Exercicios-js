521b5f22a77ddb5c46862967309f8b362cefbf62
Douglas Gomes <douglasgabriel.rdp@hotmail.com>
2023-02-08 21:41:39 -0300

    first commit

---
+++ Exercicio 4/4.js
@@ -0,0 +1,4 @@
+let valor_1 = Numer(prompt('Digite o primeiro numero :'));
+let valor_2 = Numer(prompt('Digite o segundo numero :'));
+let valor_3 = Numer(prompt('Digite o terceiro numero :'));
+
---
+++ Exercicio 6/6.js
@@ -0,0 +1,22 @@
+const trabalhadores = [
+	{
+		nome: "José",
+		salario_bruto: 3950.23,
+		cargo: "Analista de sistemas jr",
+	},
+	{
+		nome: "Maria",
+		salario_bruto: 5900,
+		cargo: "Analista de sistemas pleno",
+	},
+	{
+		nome: "João",
+		salario_bruto: 2500,
+		cargo: "estagiário",
+	},
+	{
+		nome: "Sérgio",
+		salario_bruto: 3951.50,
+		cargo: "Consultor financeiro",
+	},
+];
